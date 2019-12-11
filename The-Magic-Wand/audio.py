7ac2db5c09f78ac66d9932d54bd0599cc1856da4
madhumithatp <mato2277@colorado.edu>
2019-12-10 17:20:23 -0700

    Basic Client functionality

--- The-Magic-Wand/audio.py
+++ The-Magic-Wand/audio.py
@@ -1,27 +1,100 @@
-#!/usr/bin/python3
+``  11111111111 #!/usr/bin/python3
 
 import pyaudio
-import numpy as np
+import wave
+import boto3
+from time import sleep
+import requests
 
-#The following code comes from markjay4k as referenced below
+bucket_wave="buckwave"
+region="us-east-1"
+audioip= "test1.wav"
 
-chunk=4096
-RATE=44100
+form_1 = pyaudio.paInt16 # 16-bit resolution
+chans = 1 # 1 channel
+samp_rate = 44100 # 44.1kHz sampling rate
+chunk = 4096 # 2^12 samples for buffer
+record_secs = 3 # seconds to record
+dev_index = 2 # device index found by p.get_device_info_by_index(ii)
+wav_output_filename = 'test1.wav' # name of .wav file
 
-p=pyaudio.PyAudio()
+audio = pyaudio.PyAudio() # create pyaudio instantiation
 
-#input stream setup
-stream=p.open(format = pyaudio.paInt16,rate=RATE,channels=1, input_device_index = 2, input=True, frames_per_buffer=chunk)
+# create pyaudio stream
+stream = audio.open(format = form_1,rate = samp_rate,channels = chans, \
+                    input_device_index = dev_index,input = True, \
+                    frames_per_buffer=chunk)
+print("recording")
+frames = []
 
-#the code below is from the pyAudio library documentation referenced below
-#output stream setup
-player=p.open(format = pyaudio.paInt16,rate=RATE,channels=1, output=True, frames_per_buffer=chunk)
+# loop through stream and append audio chunks to frame array
+for ii in range(0,int((samp_rate/chunk)*record_secs)):
+    data = stream.read(chunk,exception_on_overflow = False)
+    frames.append(data)
 
-while True:            #Used to continuously stream audio
-     data=np.fromstring(stream.read(chunk,exception_on_overflow = False),dtype=np.int16)
-     player.write(data,chunk)
-    
-#closes streams
+print("finished recording")
+
+# stop the stream, close it, and terminate the pyaudio instantiation
 stream.stop_stream()
 stream.close()
-p.terminate
+audio.terminate()
+
+# save the audio frames as .wav file
+wavefile = wave.open(wav_output_filename,'wb')
+wavefile.setnchannels(chans)
+wavefile.setsampwidth(audio.get_sample_size(form_1))
+wavefile.setframerate(samp_rate)
+wavefile.writeframes(b''.join(frames))
+wavefile.close()
+
+print("Connecting to S3 bucket")
+S3 = boto3.client('s3')
+S3.upload_file(audioip,bucket,audioip) 
+
+transcribe = boto3.client('transcribe',region)
+job_name = "myjob"
+job_uri = "s3://buckwave/test1.wav"
+transcribe.delete_transcription_job(TranscriptionJobName=job_name)
+transcribe.start_transcription_job(
+    TranscriptionJobName=job_name,
+    LanguageCode='en-US',
+    MediaFormat= "wav",
+    Media= {
+        "MediaFileUri": job_uri
+    }
+) 
+while True:
+    status = transcribe.get_transcription_job(TranscriptionJobName=job_name)
+    if status['TranscriptionJob']['TranscriptionJobStatus'] in ['COMPLETED', 'FAILED']:
+        break
+    print("Not ready yet...")
+    sleep(5)
+    
+myurl = status['TranscriptionJob']['Transcript']['TranscriptFileUri']
+#print(myurl) 
+Text_Data = (requests.get(myurl).json())['results']['transcripts'][0]['transcript']
+print(Text_Data)
+
+
+
+
+'''
+		#The following code comes from markjay4k as referenced below
+		p=pyaudio.PyAudio()
+
+		#input stream setup
+		stream=p.open(format = pyaudio.paInt16,rate=RATE,channels=1, input_device_index = 2, input=True, frames_per_buffer=chunk)
+
+		#the code below is from the pyAudio library documentation referenced below
+		#output stream setup
+		player=p.open(format = pyaudio.paInt16,rate=RATE,channels=1, output=True, frames_per_buffer=chunk)
+
+		while True:            #Used to continuously stream audio
+			 data=np.fromstring(stream.read(chunk,exception_on_overflow = False),dtype=np.int16)
+			 player.write(data,chunk)
+			
+		#closes streams
+		stream .stop_stream()
+		stream.close()
+		p.terminate
+		'''
--- The-Magic-Wand/client.py
+++ The-Magic-Wand/client.py
@@ -5,25 +5,59 @@ from PyQt5.QtWidgets import QApplication, QWidget, QLabel
 from PyQt5.QtGui import QIcon, QPixmap
 from picamera import PiCamera
 from time import sleep
-import pyaudio
 import numpy as np
-
+import boto3
+from botocore.exceptions import BotoCoreError, ClientError
+from tempfile import gettempdir
+from contextlib import closing
+import subprocess
+import os
+from pygame import mixer
+import pyaudio
+import wave
+import boto3
+from time import sleep
+import requests
 
 chunk=4096
 RATE=44100
 
+bucket_wave="buckwave"
+bucket_image="buckimg"
+region="us-east-1"
+capture= "image.jpg"
+Confidence = 80
+audio_file = "speech.mp3"
+command_filename = 'identify.wav' # name of .wav file
+feedback_filename = 'feedback.wav' # name of .wav file
+identify_command = "identify"
+right_feedback = "Right"
+wrong_feedback = "Wrong"
+
+form_1 = pyaudio.paInt16 # 16-bit resolution
+chans = 1 # 1 channel
+samp_rate = 48000 # 48kHz sampling rate
+chunk = 2 # 2^12 samples for buffer
+record_time = 3 # seconds to record
+dev_index = 2 # device index found by p.get_device_info_by_index(ii)
+wav_output_filename = 'test1.wav' # name of .wav file
+
+
+audio = pyaudio.PyAudio() # create pyaudio instantiation
+
+
+
+
 class Client(QWidget):
 	camera = PiCamera()
+	
 	def __init__(self):
 		super().__init__()
-
+	"""
+	Initialize the display
+	"""
 	def main(self):
 		Client.camera.resolution = (640, 480)
-		Client.camera.start_preview()
-		sleep(5)
-		Client.camera.capture('image.jpg')
-		Client.camera.stop_preview()
-		
 		# Widget
 		self.setWindowTitle("Captured Image")
 		self.setGeometry(10, 10, 640, 480)
@@ -31,29 +65,159 @@ class Client(QWidget):
 		pixmap = QPixmap('image.jpg')
 		label.setPixmap(pixmap)
 		self.show()
-		#The following code comes from markjay4k as referenced below
-		p=pyaudio.PyAudio()
+	
+	"""
+	Capture the image
+	image is the file name of the image;
+	return object of the image
+	"""	
+	def capture_image(image ):
+		Client.camera.capture(image)
+		return image
+	
+	"""
+	Upload the content to the specified bucket
+	"""		
+	def upload_to_bucket(content,bucket):
+		S3 = boto3.client('s3')
+		S3.upload_file(content,bucket,content) 
+	
+	"""
+	Detect the labels for specified image in the specified bucket
+	returm the label with max confidence 
+	"""	
+	def detect_img_labels(image,bucket):
+		confidence= 0
+		client=boto3.client('rekognition',region)
+		print("Recognizing")
+		response = client.detect_labels(
+					Image={
+							'S3Object':{
+									'Bucket':bucket,'Name':image
+									}
+							},
+									MaxLabels=10)
+		
+			 
+		for i in response['Labels']:
+			if (confidence < i['Confidence']):
+					confidence = i['Confidence']
+					label = i['Name']
+		
+		return label
+	
+	"""
+	 Convert the text passed into a speech file specified 
+	"""	
+	
+	def text_to_speech(text,speech_file):
+		polly_client = boto3.client('polly',region)
+		try:
+			response = polly_client.synthesize_speech(VoiceId='Joanna',
+			OutputFormat='mp3', 
+			Text = text)
+		except (BotoCoreError, ClientError) as error:
+			print(error)
+			sys.exit(-1)
+		file = open(speech_file, 'wb')
+		file.write(response['AudioStream'].read())
+		file.close()
+	
+	"""
+	record audio for given number of seconds 
+	"""	
+	def record_audio(wav_output_filename,record_secs):
+		# create pyaudio stream
+		stream = audio.open(format = form_1,rate = samp_rate,channels = chans, \
+                    input_device_index = dev_index,input = True, \
+                    frames_per_buffer=chunk)
+		print("recording")
+		frames = []
 
-		#input stream setup
-		stream=p.open(format = pyaudio.paInt16,rate=RATE,channels=1, input_device_index = 2, input=True, frames_per_buffer=chunk)
+		# loop through stream and append audio chunks to frame array
+		for i in range(0,int((samp_rate/chunk)*record_secs)):
+			data = stream.read(chunk,exception_on_overflow = False)
+			if i % 3 == 0:
+				frames.append(data)
 
-		#the code below is from the pyAudio library documentation referenced below
-		#output stream setup
-		player=p.open(format = pyaudio.paInt16,rate=RATE,channels=1, output=True, frames_per_buffer=chunk)
-
-		while True:            #Used to continuously stream audio
-			 data=np.fromstring(stream.read(chunk,exception_on_overflow = False),dtype=np.int16)
-			 player.write(data,chunk)
-			
-		#closes streams
+		print("finished recording")
+		# stop the stream, close it, and terminate the pyaudio instantiation
 		stream.stop_stream()
 		stream.close()
-		p.terminate
-		sys.exit(app.exec_())
+		
+		
+		# save the audio frames as .wav file
+		wavefile = wave.open(wav_output_filename,'wb')
+		wavefile.setnchannels(chans)
+		wavefile.setsampwidth(audio.get_sample_size(form_1))
+		wavefile.setframerate(samp_rate/3)
+		wavefile.writeframes(b''.join(frames))
+		wavefile.close()
+	
+	"""
+	play audio for given input file
+	"""	
+		
+	def play_audio(input_file):
+		mixer.music.load(audio_file)
+		mixer.music.play()
+		while mixer.music.get_busy() == True:
+			continue
+		mixer.music.stop()
+	
+	"""
+	using lex bot to convert speech to text of given input wave file
+	"""	
+	def aws_lex(wavefile):
+		
+		lex_run = boto3.client('lex-runtime', region_name = region)
+		wavefile = wave.open(wavefile)
+		response = lex_run.post_content(botName = "texttospeech", 
+										botAlias = "command", 
+										contentType = "audio/l16;rate=16000; channels=1",
+										accept = "text/plain; charset=utf-8",\
+										inputStream = wavefile.readframes(96044), userId = "test")
+										
+		text_data = response['ResponseMetadata']['HTTPHeaders']['x-amz-lex-message']
+		return text_data
+	
 		
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
 	obj = Client()
 	obj.main()
-
-
+	feedback_done = 0;
+	print("Record AUDIO")
+	Client.record_audio(command_filename,3)
+	command = Client.aws_lex(command_filename)
+	
+	if identify_command in command:
+		mixer.init()
+		#Client.camera.start_preview()
+		#sleep(5)
+		capture = Client.capture_image(capture)
+		#Client.camera.stop_preview()
+		Client.upload_to_bucket(capture,bucket_image)
+		captured_label = Client.detect_img_labels(capture,bucket_image)
+		print(captured_label)
+		
+		if captured_label is not None:
+			Client.text_to_speech(captured_label,audio_file)
+			Client.play_audio(audio_file)
+		while feedback_done == 0:
+			print("Give Feedback")
+			Client.record_audio(feedback_filename,3)
+			command = Client.aws_lex(feedback_filename)
+			print(command)
+			if right_feedback in command:
+				print("Rightly detected object")
+				feedback_done = 1
+			elif wrong_feedback in command:
+				print("Wrongly Detected Object")
+				feedback_done = 1
+			else:
+				feedback_done = 0;
+	else:
+		print("retry recording audio")
+	
+	sys.exit(app.exec_())
